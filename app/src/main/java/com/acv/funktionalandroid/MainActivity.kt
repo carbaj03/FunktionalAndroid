81f1d1f955236a5c5bd2d2527b451f13e1e366ae
carbaj0 <carbaj03@gmail.com>
2017-08-29 19:30:32 +0200

    first

---
+++ app/src/main/java/com/acv/funktionalandroid/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.acv.funktionalandroid
+
+class Cafe {
+    fun buyCoffee(cc: CreditCard): Coffee {
+        val cup = Coffee(20.0)
+        //This is the side effect
+        cc.charge(cup.price)
+        return cup
+    }
+}
+
+data class Coffee(val price: Double)
+
+interface CreditCard {
+    fun charge(value: Double)
+}
